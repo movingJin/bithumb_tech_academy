8300f30541fa6406ffd0cb9fbf349171a2e62c8c
movingjin <shdlehdwls@gmail.com>
2021-08-22 21:15:40 +0900

    delete prefix mvc

--- mvc/src/main/java/net/movingjin/api/item/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package net.movingjin.api.item.repository;
-
-import net.movingjin.api.item.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- mvc/src/main/java/net/movingjin/api/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.movingjin.api.user.repository;
-
-import net.movingjin.api.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
